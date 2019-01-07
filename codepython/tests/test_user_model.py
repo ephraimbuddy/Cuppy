f35c871843ec26d24100c37f55887e52c26d0882
EphraimBuddy <splendidzigy24@gmail.com>
2019-01-07 20:30:10 +0100

    use dummy email

--- codepython/tests/test_user_model.py
+++ codepython/tests/test_user_model.py
@@ -4,7 +4,7 @@ from codepython.tests import BaseTest
 class TestSetPassword(BaseTest):
 
     def test_password_hash_saved(self):
-        user = self.makeUser(username="ephraim", email='ephraim@gmail.com')
+        user = self.makeUser(username="ephraim", email='example@example.com')
         self.assertFalse(user.password_hash)
 
         user.set_password("secret")
@@ -14,18 +14,18 @@ class TestSetPassword(BaseTest):
 class TestCheckPassword(BaseTest):
 
     def test_password_hash_not_set(self):
-        user = self.makeUser(username="ephraim", email='ephraim@gmail.com')
+        user = self.makeUser(username="ephraim", email='example@example.com')
         self.assertFalse(user.password_hash)
         self.assertFalse(user.check_password('secret'))
 
     def test_correct_password(self):
-        user = self.makeUser(username="ephraim", email='ephraim@gmail.com')
+        user = self.makeUser(username="ephraim", email='example@example.com')
         user.set_password('secret')
         self.assertTrue(user.password_hash)
         self.assertTrue(user.check_password('secret'))
 
     def test_incorrect_password(self):
-        user = self.makeUser(username="ephraim", email='ephraim@gmail.com')
+        user = self.makeUser(username="ephraim", email='example@example.com')
         user.set_password('secret')
         self.assertTrue(user.password_hash)
         self.assertFalse(user.check_password('incorrect'))
@@ -36,7 +36,7 @@ class TestAuthUserLog(BaseTest):
     def test_AddUserLog(self):
         from codepython.models.users import AuthUserLog
         from sqlalchemy.sql import func
-        user = self.makeUser(username="ephraim", email='ephraim@gmail.com')
+        user = self.makeUser(username="ephraim", email='example@example.com')
         user.set_password('secret')
         log = AuthUserLog(
             user = user,
@@ -51,7 +51,7 @@ class TestActivityLog(BaseTest):
     def test_userActivity(self):
         from codepython.models.users import UserActivity
         from sqlalchemy.sql import func
-        user = self.makeUser(username="ephraim", email='ephraim@gmail.com')
+        user = self.makeUser(username="ephraim", email='example@example.com')
         user.set_password('secret')
         ac = UserActivity(
             user=user,
